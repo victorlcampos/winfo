6b7463b9ab6a60e5e59fa3a8407aa7583d5ef1d9
Victor Campos <victorcampos@ufrj.br>
2013-07-15 13:19:41 +0000

    adding smtp config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,15 @@ InitialProject::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.default_url_options = { host: 'www.winfo.me' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: :plain,
+    user_name: "winfojenkins@gmail.com",
+    password: "jenkins123",
+    enable_starttls_auto: true
+  }
 end
